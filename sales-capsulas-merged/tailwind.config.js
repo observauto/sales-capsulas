17ec5fd7aff3eef8d9be55c001f6324ad27738e0
observauto <felipegaran@observauto.com>
2025-10-14 18:56:19 -0500

    Delete sales-capsulas-merged directory

--- sales-capsulas-merged/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** Tailwind config (solo asegura darkMode: 'class') */
-export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: { extend: {} },
-  plugins: [],
-};
